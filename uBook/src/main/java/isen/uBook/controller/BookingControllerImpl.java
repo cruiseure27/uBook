19cba57afd7661a0850aeefbb98a4feec32eba75
cruiseure27 <manunush@free.fr>
2016-01-18 12:12:03 +0100

    Ajout des champs dans le formulaire de réservation

--- uBook/src/main/java/isen/uBook/controller/BookingControllerImpl.java
+++ uBook/src/main/java/isen/uBook/controller/BookingControllerImpl.java
@@ -7,39 +7,52 @@ import org.springframework.web.servlet.ModelAndView;
 
 import isen.uBook.model.Booking;
 import isen.uBook.repository.BookingRepository;
+import isen.uBook.repository.MemberRepository;
+import isen.uBook.repository.PlaceRepository;
+import isen.uBook.repository.RessourceRepository;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class BookingControllerImpl implements BookingController{
 	
 	@Autowired
 	private BookingRepository bookingRepository;
+	@Autowired
+	private MemberRepository memberRepository;
+	@Autowired
+	private PlaceRepository placeRepository;
+	@Autowired
+	private RessourceRepository ressourceRepository;
 	
 	static String VIEW_NAME = "/uBook/booking";
 	
 	@Override
 	public ModelAndView crudChange(Booking booking, BindingResult bindingResult) {
-		ModelAndView view = new ModelAndView(VIEW_NAME);
-		System.out.println(booking.getMember().getFirstName());
+		log.info(booking.getMember().getId().toString());
 		if (bindingResult.hasErrors()) {
-			//log.warn("There are errors! " + bindingResult);
-			return view;
+			log.warn("There are errors! " + bindingResult);
+			return getBookingList();
 		}
 		bookingRepository.save(booking);
-		//view.addObject("booking", new Booking());
-		return view;
+		return getBookingList();
 	}
 
 	@Override
 	public ModelAndView getBookingList() {
 		ModelAndView view = new ModelAndView(VIEW_NAME);
 		view.addObject("booking", new Booking());
+		view.addObject("bookingsList",bookingRepository.findAll());
+		view.addObject("membersList", memberRepository.findAll());
+		view.addObject("placesList", placeRepository.findAll());
+		view.addObject("ressourcesList", ressourceRepository.findAll());
 		return view;
 		
 	}
 
 	@Override
 	public Booking getBooking(Integer id) {
-		return null;//(Booking) bookingRepository.findOne(id);
+		return (Booking) bookingRepository.findOne(id);
 	}
 
 }
